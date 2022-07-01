2c10705ded972dafd8b60f184b209ec8ce4fb13d
JakubCisowski <jakub.cisowski1@gmail.com>
2022-07-01 22:40:14 +0200

    Make searchbar dynamic - it now automatically refreshes search filter every time text in searchbar is edited.

--- SpotifyLibraryManager/SpotifyLibraryManager/Pages/ToolBarPage.xaml.cs
+++ SpotifyLibraryManager/SpotifyLibraryManager/Pages/ToolBarPage.xaml.cs
@@ -3,32 +3,37 @@ using System.Windows.Input;
 
 namespace SpotifyLibraryManager.Pages
 {
-    /// <summary>
-    /// Interaction logic for ToolBarPage.xaml
-    /// </summary>
-    public partial class ToolBarPage : Page
-    {
-        public ToolBarPage()
-        {
-            InitializeComponent();
-        }
+	/// <summary>
+	/// Interaction logic for ToolBarPage.xaml
+	/// </summary>
+	public partial class ToolBarPage : Page
+	{
+		public ToolBarPage()
+		{
+			InitializeComponent();
+		}
 
-        private void SearchText_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                SearchBtn.Command.Execute(SearchText.Text);
-                Keyboard.ClearFocus();
-            }
-            else if (e.Key == Key.Escape)
-            {
-                Keyboard.ClearFocus();
-            }
-        }
+		private void SearchText_PreviewKeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Enter)
+			{
+				SearchBtn.Command.Execute(SearchText.Text);
+				Keyboard.ClearFocus();
+			}
+			else if (e.Key == Key.Escape)
+			{
+				Keyboard.ClearFocus();
+			}
+		}
 
-        private void SearchText_GotKeyboardFocus(object sender, System.Windows.RoutedEventArgs e)
-        {
-            SearchText.Dispatcher.BeginInvoke(() => SearchText.SelectAll());
-        }
-    }
+		private void SearchText_GotKeyboardFocus(object sender, System.Windows.RoutedEventArgs e)
+		{
+			SearchText.Dispatcher.BeginInvoke(() => SearchText.SelectAll());
+		}
+
+		private void SearchText_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			SearchBtn.Command.Execute(SearchText.Text);
+		}
+	}
 }
