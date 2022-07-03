fadd5178cde322fcb0cd489c233a3d4a57177f5d
Jerzy <jurekbalcer@gmail.com>
2022-07-03 14:33:58 +0200

    centered combobox popup

---
+++ SpotifyLibraryManager/SpotifyLibraryManager/Helpers/Converters/CenterPopupConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SpotifyLibraryManager.Helpers.Converters
+{
+    public class CenterPopupConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            double popupWidth = (double)values[0];
+            double toggleButtonWidth = (double)values[1];
+
+            return -(popupWidth - toggleButtonWidth) / 2d;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
