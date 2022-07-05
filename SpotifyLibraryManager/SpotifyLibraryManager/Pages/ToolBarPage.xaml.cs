510573501257ddff8b016267dda010333df348af
JakubCisowski <jakub.cisowski1@gmail.com>
2022-07-05 22:09:12 +0200

    Add dynamic search.

--- SpotifyLibraryManager/SpotifyLibraryManager/Pages/ToolBarPage.xaml.cs
+++ SpotifyLibraryManager/SpotifyLibraryManager/Pages/ToolBarPage.xaml.cs
@@ -1,16 +1,23 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace SpotifyLibraryManager.Pages
 {
-	/// <summary>
-	/// Interaction logic for ToolBarPage.xaml
-	/// </summary>
 	public partial class ToolBarPage : Page
 	{
+		// Lower value -> faster search | Higher value -> more optimal
+		private const int SEARCH_INTERVAL_MS = 100;
+
+		private DispatcherTimer searchCheckTimer;
+		private bool isTimerOn = false;
+		private string lastSearchText;
+
 		public ToolBarPage()
 		{
 			InitializeComponent();
+			SetupSearchTimer();
 		}
 
 		private void SearchText_PreviewKeyDown(object sender, KeyEventArgs e)
@@ -31,9 +38,52 @@ namespace SpotifyLibraryManager.Pages
 			SearchText.Dispatcher.BeginInvoke(() => SearchText.SelectAll());
 		}
 
-		//private void SearchText_TextChanged(object sender, TextChangedEventArgs e)
-		//{
-		//	SearchBtn.Command.Execute(SearchText.Text);
-		//}
+		private void SearchText_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			if (isTimerOn == false)
+			{
+				// If search text changed and timer is off, start the timer.
+				// Timer checks every <SEARCH_INTERVAL_MS> whether search text has changed compared to last check.
+				// If not, it automatically executes search.
+				StartSearchTimer(SearchText.Text);
+			}
+		}
+
+		private void SetupSearchTimer()
+		{
+			searchCheckTimer = new DispatcherTimer
+			{
+				Interval = new TimeSpan(0, 0, 0, 0, SEARCH_INTERVAL_MS)
+			};
+			searchCheckTimer.Tick += SearchTimer_Tick;
+		}
+
+		private void StartSearchTimer(string searchText)
+		{
+			lastSearchText = searchText;
+			searchCheckTimer.Start();
+			isTimerOn = true;
+		}
+
+		private void StopSearchTimer()
+		{
+			searchCheckTimer.Stop();
+			isTimerOn = false;
+		}
+
+		private void SearchTimer_Tick(object source, EventArgs e)
+		{
+			bool isSearchTextChanged = lastSearchText != SearchText.Text;
+
+			if (isSearchTextChanged)
+			{
+				lastSearchText = SearchText.Text;
+			}
+			else
+			{
+				StopSearchTimer();
+				SearchBtn.Command.Execute(SearchText.Text);
+			}
+		}
 	}
 }
